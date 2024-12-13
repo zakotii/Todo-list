73fc31823eb811ea718e15f2cf6a8ed9f8c87122
zakotii <abc1234567892777@gmail.com>
2024-12-13 07:28:39 +0200

    Clean up .gitignore: Remove unnecessary entries

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -20,15 +20,4 @@ from todo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
-    #path('', views.home, name='home'),
-    #path('add-task/', views.add_task, name='add_task'),
-    #path('update-task/<int:pk>/', views.update_task, name='update_task'),
-    #path('delete-task/<int:pk>/', views.delete_task, name='delete_task'),
-    #path('toggle-task-status/<int:pk>/', views.toggle_task_status, name='toggle_task_status'),
-    #path('tags/', views.tag_list, name='tag_list'),
-
-    # Пути для работы с тегами
-    #path('tags/add/', views.add_tag, name='add_tag'),
-    #path('tags/update/<int:pk>/', views.update_tag, name='update_tag'),  # Путь для обновления тега
-    #path('tags/delete/<int:pk>/', views.delete_tag, name='delete_tag'),  # Путь для удаления тега
 ]
